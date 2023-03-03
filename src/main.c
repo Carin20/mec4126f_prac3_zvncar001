c340fea8b2805829da351d98a4e8fbefd04ce9fe
Carin20 <100944830+Carin20@users.noreply.github.com>
2023-03-03 15:15:56 +0200

    Update main.c
    Q3.1

--- src/main.c
+++ src/main.c
@@ -14,12 +14,22 @@
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
 void main(void);                                                   //COMPULSORY
+struct age_data
+{
+	uint8_t date; //collect day/date of the month
+	uint8_t month; //collect month number
+	uint16_t year; //collect year of birth
+	uint8_t age; //collect age
+}my_age;
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 void main(void)
 {
-
+    my_age.date = 20; //collect day/date of the month
+    my_age.month = 05; //collect month number
+    my_age.year = 2000; //collect year of birth
+    my_age.age = 22; //collect age
 
 	while(1)
 	{
